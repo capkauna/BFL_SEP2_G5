dd8e00b91ca1502232b4adc2d965bd3a364a7208
Marabooo <354536@viauc.dk>
2025-05-12 15:34:26 +0200

    Added basic Request & Response (java serialized objects) Basic Login (hardcoded) Client to Server Renamed Typo (LoginServlet)

--- Shared/src/dto/Request.java
+++ Shared/src/dto/Request.java
@@ -1,5 +1,20 @@
 package dto;
 
-public class Request
-{
+import java.io.Serializable;
+
+public class Request implements Serializable {
+  private String command;
+  private String username;
+  private String password;
+
+  public Request(String command, String username, String password) {
+    this.command = command;
+    this.username = username;
+    this.password = password;
+  }
+
+  public String getCommand() { return command; }
+  public String getUsername() { return username; }
+  public String getPassword() { return password; }
 }
+
