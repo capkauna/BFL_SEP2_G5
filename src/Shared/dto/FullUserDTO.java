4f9bb9c25787a5f7bb29d06c877e8f0c7c2924e5
Cristina Matei <354776@via.dk>
2025-05-24 22:42:34 +0200

    UserPageView - fxml, controller + in ViewModel class UserProfile / in shared/dto are the classes for VM // this classes is meant specifically for the main user view

--- src/Shared/dto/FullUserDTO.java
+++ src/Shared/dto/FullUserDTO.java
@@ -3,7 +3,7 @@ package Shared.dto;
 // this is meant specifically for the main user view
 public class FullUserDTO
 {
-  private String userName, fullName, email,phoneNumber, address, avatar;
+  private String userName, fullName, email,phoneNumber, address, avatar, moreInfo;
 
   public FullUserDTO(String userName, String fullName, String email,  String phoneNumber, String address, String avatar)
   {
@@ -13,5 +13,34 @@ public class FullUserDTO
     this.phoneNumber = phoneNumber;
     this.address = address;
     this.avatar = avatar;
+    this.moreInfo = moreInfo;
   }
+  public String getUserName() {
+    return userName;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String  getAvatar() {
+    return avatar;
+  }
+
+  public String getMoreInfo() {
+    return moreInfo;
+  }
+
 }
